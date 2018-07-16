e4156c13fe415cb5f9f8242aff3d26a19751091a
Vikas Shetty <vikasrs4u@gmail.com>
2018-07-16 00:03:01 -0700

    commiting the changes

--- Weather-App/Controller/WeatherViewController.swift
+++ Weather-App/Controller/WeatherViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherViewController.swift
 //  Weather-App
 //
 //  Created by Vikas R S on 7/15/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

ce7e0ee97e963c06425c9d74d7201a694ecadef1
Vikas Shetty <vikasrs4u@gmail.com>
2018-07-18 19:28:45 -0700

    Initial file changes with pods set up.
    All the app related image assets and pods are added to the project.

--- Weather-App/Controller/WeatherViewController.swift
+++ Weather-App/Controller/WeatherViewController.swift
@@ -8,18 +8,94 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
 
+class WeatherViewController: UIViewController {
+    
+    //Constants
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "31e7d3b0d44002435656d630745bf6f8"
+    
+    
+    //TODO: Declare instance variables here
+    
+    
+    
+    //Pre-linked IBOutlets
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //TODO:Set up the location manager here.
+        
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
+    
+    //MARK: - Networking
+    /***************************************************************/
+    
+    //Write the getWeatherData method here:
+    
+    
+    
+    
+    
+    
+    
+    //MARK: - JSON Parsing
+    /***************************************************************/
+    
+    
+    //Write the updateWeatherData method here:
+    
+    
+    
+    
+    
+    //MARK: - UI Updates
+    /***************************************************************/
+    
+    
+    //Write the updateUIWithWeatherData method here:
+    
+    
+    
+    
+    
+    
+    //MARK: - Location Manager Delegate Methods
+    /***************************************************************/
+    
+    
+    //Write the didUpdateLocations method here:
+    
+    
+    
+    //Write the didFailWithError method here:
+    
+    
+    
+    
+    
+    //MARK: - Change City Delegate methods
+    /***************************************************************/
+    
+    
+    //Write the userEnteredANewCityName Delegate method here:
+    
+    
+    
+    //Write the PrepareForSegue Method here
+    
+    
+    
+    
+    
 }
-
